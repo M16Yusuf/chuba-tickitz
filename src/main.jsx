6f7f729ef9e48135252bcf050e9da5d656c1be87
M16yusuf <yusufsmd58@gmail.com>
2025-07-30 06:43:15 +0700

    feat(main-page): make simple navigation with select html

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { useState, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/styles/pages/index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// import Home from "./components/Home.jsx";
+
+import Login from "./components/pages/LoginPage.jsx";
+import Register from "./components/pages/RegisterPage.jsx";
+import Movies from "./components/pages/MoviesPages.jsx";
+
+export default function Home() {
+  const [page, setPage] = useState("dom");
+  function changeHandler(event) {
+    setPage(event.target.value);
+  }
+  return (
+    <>
+      <select name="page" value={page} onChange={changeHandler}>
+        <option value="login">Login</option>
+        <option value="register">Register</option>
+        <option value="movies">Movies</option>
+      </select>
+      {page === "login" && <Login />}
+      {page === "register" && <Register />}
+      {page === "movies" && <Movies />}
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Home />
+  </StrictMode>
+  // <Login />
+  // <Register />
+  // <Movies />
+);
