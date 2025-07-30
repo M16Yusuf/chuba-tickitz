8679a9aa007b45cb95127a53181438793b371bdc
M16yusuf <yusufsmd58@gmail.com>
2025-07-30 10:56:40 +0700

    feat(register-validation): add register password validation

--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -1,8 +1,70 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 import "../../assets/styles/pages/register-login.css";
 
 function RegisterPage() {
+  const inputData = {};
+  const [formData, setFormData] = useState([]);
+  const [error, setError] = useState([]);
+
+  function validationInput(ev) {
+    ev.preventDefault();
+    const formD = ev.target;
+
+    const mustHvChar = /[a-zA-Z]/;
+    const mustHvDigit = /\d/;
+    const mustHvSimbol = /[!@#$%^&*/><]/;
+
+    if (!mustHvChar.test(formD.value)) {
+      if (!error.includes("char"))
+        setError((dataError) => [...dataError, "char"]);
+    } else {
+      let index = error.indexOf("char");
+      if (index !== -1) {
+        setError((dataError) => dataError.splice(index, 1));
+      }
+    }
+    if (!mustHvDigit.test(formD.value)) {
+      if (!error.includes("digit"))
+        setError((dataError) => [...dataError, "digit"]);
+    } else {
+      let index = error.indexOf("digit");
+      if (index !== -1) {
+        setError((dataError) => dataError.splice(index, 1));
+      }
+    }
+    if (!mustHvSimbol.test(formD.value)) {
+      if (!error.includes("simbol"))
+        setError((dataError) => [...dataError, "simbol"]);
+    } else {
+      let index = error.indexOf("simbol");
+      if (index !== -1) {
+        setError((dataError) => dataError.splice(index, 1));
+      }
+    }
+    if (formD.value.length < 8) {
+      if (!error.includes("length"))
+        setError((dataError) => [...dataError, "length"]);
+    } else {
+      let index = error.indexOf("length");
+      if (index !== -1) {
+        setError((dataError) => dataError.splice(index, 1));
+      }
+    }
+    if (formD.value.length == " ") {
+      if (!error.includes("empty"))
+        setError((dataError) => [...dataError, "empty"]);
+    } else {
+      let index = error.indexOf("empty");
+      if (index !== -1) {
+        setError((dataError) => dataError.splice(index, 1));
+      }
+    }
+
+    console.log(error);
+    return error;
+  }
+
   return (
     <section>
       <div className="logo">
@@ -43,7 +105,9 @@ function RegisterPage() {
               name="pass"
               placeholder="Enter your Passwod"
               id="pass"
+              onChange={validationInput}
             />
+            <ErrorProcess data={error} />
           </div>
           <div>
             <input type="checkbox" name="agree" id="agree" />
@@ -69,6 +133,62 @@ function RegisterPage() {
         </div>
       </form>
     </section>
+  );
+}
+
+function ErrorProcess(props) {
+  return (
+    <div>
+      {props.data.length > 0 &&
+        props.data.map((eleError, idx) => {
+          switch (eleError) {
+            case "char":
+              return (
+                <>
+                  <span style={{ color: "red" }} className="font-bold text-sm text-red-600" key={idx}>
+                    ❌ The password must contain characters !
+                  </span>
+                  <br />
+                </>
+              );
+            case "digit":
+              return (
+                <>
+                  <span style={{ color: "red" }} className="font-bold text-sm text-red-600"  key={idx}>
+                    ❌ The password must contain numbers !
+                  </span>
+                  <br />
+                </>
+              );
+            case "simbol":
+              return (
+                <>
+                  <span style={{ color: "red" }} className="font-bold text-sm text-red-600" key={idx}>
+                    {`❌ The passwords must contain symbols ' !@#$%^&*/><' `}
+                  </span>
+                </>
+              );
+            case "length":
+              return (
+                <>
+                  <span style={{ color: "red" }} className="font-bold text-sm text-red-600"  key={idx}>
+                    ❌ The password must contain 8 characters !
+                  </span>
+                <br />
+                </>
+              );
+            case "empty":
+              return (
+                <>
+                  <span style={{ color: "red" }} className="font-bold text-sm text-red-600"  key={idx}>
+                    ❌ The password cannot be empty !
+                  </span>
+                  <br />
+                </>
+              );
+          }
+        })}
+    </div>
   );
 }
 
