700cb0db1f4c8c4de97b94e91855c3103440d323
M16yusuf <yusufsmd58@gmail.com>
2025-08-02 17:12:23 +0700

    refactor(subscribe-component): change style for subscribe component

--- src/components/organism/SectionSubscribe.jsx
+++ src/components/organism/SectionSubscribe.jsx
@@ -2,29 +2,29 @@ import React from "react";
 
 function SectionSubscribe() {
   return (
-    <main className="p-5">
-      <section className="relative bg-blue-700 rounded-3xl py-20 p-5 flex flex-col">
-        <span className="text-white text-3xl text-center font-normal">
+    <main className="md:w-custom-max-main p-5 md:justify-self-center md:p-20">
+      <section className="relative flex flex-col overflow-hidden rounded-3xl bg-blue-700 p-5 py-20">
+        <span className="text-center text-3xl font-normal text-white">
           Subscribe to our newsletter
         </span>
         <form>
-          <div className="flex flex-col gap-4 md:flex-row mt-5">
+          <div className="mt-5 flex flex-col gap-4 md:flex-row md:justify-center">
             <input
-              className="w-full p-4 bg-[#2563eb] border border-white rounded-md text-white md:max-w-[230px]"
+              className="w-full rounded-md border border-white bg-[#2563eb] p-4 text-white md:max-w-[230px]"
               type="text"
               placeholder="First name"
             />
             <input
-              className="w-full p-4 bg-[#2563eb] border border-white rounded-md text-white md:max-w-[230px]"
+              className="w-full rounded-md border border-white bg-[#2563eb] p-4 text-white md:max-w-[230px]"
               type="text"
               placeholder="Email address"
             />
-            <button className="bg-white p-4 text-blue-700 rounded-md w-full md:max-w-[230px]">
+            <button className="w-full rounded-md bg-white p-4 text-blue-700 md:max-w-[230px]">
               Subscibe Now
             </button>
           </div>
         </form>
-        <div className="absolute bottom-[-40px] right-[-40px] w-32 h-32 border-4 border-white rounded-full"></div>
+        <div className="absolute right-[-40px] bottom-[-40px] h-32 w-32 rounded-full border-4 border-white"></div>
       </section>
     </main>
   );
