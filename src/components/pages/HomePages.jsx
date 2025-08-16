1c9726426d940520bc4b074324e2aea244f7f5d1
M16yusuf <yusufsmd58@gmail.com>
2025-08-16 19:56:35 +0700

    refactor(home-pages): use loading circle component, change variable name

--- src/components/pages/HomePages.jsx
+++ src/components/pages/HomePages.jsx
@@ -2,7 +2,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
+// component
 import CardMovie from "./../organism/CardMovie";
+import LoadingCircle from "./../organism/LoadingCircle";
 
 function HomePages() {
   const [airing, setAiring] = useState([]);
@@ -47,7 +49,7 @@ function HomePages() {
           ({ id, title, poster_path, genre_ids }) => ({
             id: id,
             title: title,
-            poster: poster_path,
+            poster_path: poster_path,
             genres: genre_ids.map((genID) => {
               const genMatch = resultGenres.find(({ id }) => id === genID);
               return genMatch != undefined ? genMatch.name : "unknown";
@@ -59,7 +61,7 @@ function HomePages() {
           ({ id, title, poster_path, genre_ids }) => ({
             id: id,
             title: title,
-            poster: poster_path,
+            poster_path: poster_path,
             genres: genre_ids.map((genID) => {
               const genMatch = resultGenres.find(({ id }) => id === genID);
               return genMatch != undefined ? genMatch.name : "unknown";
@@ -77,27 +79,7 @@ function HomePages() {
 
   return (
     <>
-      {isLoading && (
-        <div role="status" className="flex flex-row justify-center">
-          <svg
-            aria-hidden="true"
-            className="h-[400px] w-[400px] animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span className="sr-only">Loading...</span>
-        </div>
-      )}
+      {isLoading && <LoadingCircle></LoadingCircle>}
       {!isLoading && Object.keys(airing).length > 0 && (
         <main className="w-full p-3 md:max-w-[1440px] md:justify-self-center md:px-20 md:py-10">
           <section className="flex flex-col gap-5 md:flex-row md:justify-center">
@@ -131,22 +113,22 @@ function HomePages() {
             {airing && (
               <div className="grid h-[300px] w-full grid-cols-2 grid-rows-[minmax(0,2fr)_minmax(0,1fr)_minmax(0,2fr)] gap-2 sm:h-[360px] sm:gap-[6px] md:h-[400px] md:gap-[8px] lg:w-5/12">
                 <img
-                  src={`${import.meta.env.VITE_PREFIX_IMG_TMDB}/${airing[0].poster}`}
+                  src={`${import.meta.env.VITE_PREFIX_IMG_TMDB}/${airing[0].poster_path}`}
                   alt="hero-1"
                   className="col-start-1 col-end-2 row-start-1 row-end-2 h-full w-full rounded-t-[12px] object-cover object-[center_30%] sm:rounded-t-[16px] md:rounded-t-[20px]"
                 />
                 <img
-                  src={`${import.meta.env.VITE_PREFIX_IMG_TMDB}/${airing[1].poster}`}
+                  src={`${import.meta.env.VITE_PREFIX_IMG_TMDB}/${airing[1].poster_path}`}
                   alt="hero-2"
                   className="col-start-2 col-end-3 row-start-1 row-end-3 h-full w-full rounded-t-[12px] object-cover object-top sm:rounded-t-[16px] md:rounded-t-[20px]"
                 />
                 <img
-                  src={`${import.meta.env.VITE_PREFIX_IMG_TMDB}/${airing[2].poster}`}
+                  src={`${import.meta.env.VITE_PREFIX_IMG_TMDB}/${airing[2].poster_path}`}
                   alt="hero-3"
                   className="col-start-1 col-end-2 row-start-2 row-end-4 h-full w-full rounded-b-[12px] object-cover object-top sm:rounded-b-[16px] md:rounded-b-[20px]"
                 />
                 <img
-                  src={`${import.meta.env.VITE_PREFIX_IMG_TMDB}/${airing[3].poster}`}
+                  src={`${import.meta.env.VITE_PREFIX_IMG_TMDB}/${airing[3].poster_path}`}
                   alt="hero-4"
                   className="col-start-2 col-end-3 row-start-3 row-end-4 h-full w-full rounded-b-[12px] object-cover object-center sm:rounded-b-[16px] md:rounded-b-[20px]"
                 />
