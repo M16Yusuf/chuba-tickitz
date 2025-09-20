cf0052e0a568306715149d357d7e9354da783cb6
M16yusuf <yusufsmd58@gmail.com>
2025-09-20 15:27:33 +0700

    refactor (cardhistory component) : change displau data from redux instead dummy data

--- src/components/organism/CardHistory.jsx
+++ src/components/organism/CardHistory.jsx
@@ -1,49 +1,55 @@
 import { useState } from "react";
+import QRCode from "react-qr-code";
 
 function CardHistory({ dataHistory }) {
   const [isToggleExp, setToggleExp] = useState(false);
 
   function logoCinema(place) {
-    switch (place) {
+    switch (place.toLowerCase()) {
       case "ebv":
         return "/logo-ebv_id.svg";
-      case "cineone21":
+      case "cineone":
         return "/logo-CineOne21.svg";
-      case "hilfix":
-        return "/logo-hilfix2.svg";
+      case "hiflix":
+        return "/logo-hiflix2.svg";
+      case "cinepolis":
+        return "/logo-cinepolis.png";
     }
   }
 
-  function isPasDate(dataDate) {
-    const cleanedString = dataDate.replace(/^.*?,\s*/, "").replace(" - ", " ");
-    const thisdate = new Date(cleanedString);
+  function formatDateAndCheckPast(dateString) {
+    const date = new Date(dateString);
 
-    const now = new Date();
-    return thisdate < now;
-  }
+    const formattedDate = date
+      .toLocaleString("en-US", {
+        weekday: "long",
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+        timeZone: "Asia/Jakarta",
+      })
+      .replace(",", "")
+      .replace(" at ", " - ");
 
-  function dateFromData(getDate) {
-    const cleanedString = getDate.replace(/^.*?,\s*/, "").replace(" - ", " ");
-    const date = new Date(cleanedString);
-
-    const day = date.getDate();
-    const month = date.toLocaleString("id-ID", { month: "long" });
-    const year = date.getFullYear();
-    return `${day} ${month} ${year}`;
-  }
-
-  function timeFromData(getData) {
-    const cleanedString = getData.replace(/^.*?,\s*/, "").replace(" - ", " ");
-    const date = new Date(cleanedString);
-
-    // const time = date.toLocaleTimeString([], {
-    //   hour: "2-digit",
-    //   minute: "2-digit",
-    // });
-
-    const time = date.toLocaleTimeString("en-US");
-
-    return time;
+    return {
+      formattedDate,
+      dateOnly: date.toLocaleDateString("id-ID", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+        timeZone: "Asia/Jakarta",
+      }),
+      timeOnly: date.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+        timeZone: "Asia/Jakarta",
+      }),
+      isPast: date < new Date(),
+    };
   }
 
   return (
@@ -51,11 +57,13 @@ function CardHistory({ dataHistory }) {
       <div className="border-background flex flex-col gap-2.5 rounded-t-2xl border-b-1 bg-white p-5 shadow-md md:w-full md:flex-row-reverse md:justify-between md:rounded-t-3xl">
         <img
           className="w-24"
-          src={logoCinema(dataHistory.place)}
+          src={logoCinema(dataHistory.cinema_name)}
           alt={dataHistory.place}
         />
         <div className="flex flex-col gap-2.5">
-          <span className="text-label text-[13px]">{dataHistory.date}</span>
+          <span className="text-label text-[13px]">
+            {`${dataHistory.city_name}, ${formatDateAndCheckPast(dataHistory.schedule_time).formattedDate}`}
+          </span>
           <span className="text-lg">{dataHistory.movie_title}</span>
         </div>
       </div>
@@ -64,14 +72,14 @@ function CardHistory({ dataHistory }) {
         <div className="flex flex-col items-center justify-between gap-5 md:flex-row">
           <div className="flex w-full flex-col items-center gap-5 md:flex-row">
             <span
-              className={`${isPasDate(dataHistory.date) ? "text-label bg-[rgba(110,113,145,0.2)]" : "text-active bg-[rgba(0,186,136,0.2)]"} w-full rounded-lg p-2.5 text-center md:block md:max-w-44`}
+              className={`${formatDateAndCheckPast(dataHistory.schedule_time).isPast ? "text-label bg-[rgba(110,113,145,0.2)]" : "text-active bg-[rgba(0,186,136,0.2)]"} w-full rounded-lg p-2.5 text-center md:block md:max-w-44`}
             >
-              {`${isPasDate(dataHistory.date) ? "Ticked used" : "Ticket is Active"}`}
+              {`${formatDateAndCheckPast(dataHistory.schedule_time).isPast ? "Ticked used" : "Ticket is Active"}`}
             </span>
             <span
-              className={`${dataHistory.is_paid ? "text-blue-primary bg-[rgba(29,78,216,0.2)]" : "text-importan bg-[rgba(232,44,44,0.2)]"} block w-full rounded-lg p-2.5 text-center md:block md:max-w-44`}
+              className={`${dataHistory.paid_at != null ? "text-blue-primary bg-[rgba(29,78,216,0.2)]" : "text-importan bg-[rgba(232,44,44,0.2)]"} block w-full rounded-lg p-2.5 text-center md:block md:max-w-44`}
             >
-              {dataHistory.is_paid ? "Paid" : "Not paid"}
+              {dataHistory.paid_at != null ? "Paid" : "Not paid"}
             </span>
           </div>
           <div
@@ -92,13 +100,14 @@ function CardHistory({ dataHistory }) {
           className={`${!isToggleExp && "hidden"} mt-5 flex flex-col gap-2.5`}
         >
           <div className="text-lg font-bold">Ticket Information</div>
-          {dataHistory.is_paid ? (
+          {dataHistory.paid_at != null ? (
             <div className="flex flex-col md:flex-row">
-              <img
-                className="w-32"
-                src={dataHistory.data}
-                alt={dataHistory.data}
-              />
+              {dataHistory.code_ticket != null && (
+                <QRCode
+                  value={dataHistory.code_ticket}
+                  className="m-0.5 w-36"
+                />
+              )}
               <div className="grid grid-cols-3 gap-2.5">
                 <div className="flex flex-col">
                   <span className="text-label text-[12px]">Category</span>
@@ -109,16 +118,16 @@ function CardHistory({ dataHistory }) {
                 <div className="flex flex-col">
                   <span className="text-label text-[12px]">Time</span>
                   <span className="text-sm font-semibold">
-                    {timeFromData(dataHistory.date)}
+                    {formatDateAndCheckPast(dataHistory.schedule_time).timeOnly}
                   </span>
                 </div>
                 <div className="flex flex-col">
                   <span className="text-label text-[12px]">Seats</span>
                   <span className="text-sm font-semibold">
-                    {dataHistory.seat.map((seat) => `${seat}, `)}
+                    {dataHistory.seats.map((seat) => `${seat.seat_code}, `)}
                   </span>
                 </div>
-                <div className="flex flex-col">
+                <div className="flex max-w-[200px] flex-col">
                   <span className="text-label text-[12px]">Movie</span>
                   <span className="truncate text-sm font-semibold">
                     {dataHistory.movie_title}
@@ -127,13 +136,13 @@ function CardHistory({ dataHistory }) {
                 <div className="flex flex-col">
                   <span className="text-label text-[12px]">Date</span>
                   <span className="text-sm font-semibold">
-                    {dateFromData(dataHistory.date)}
+                    {formatDateAndCheckPast(dataHistory.schedule_time).dateOnly}
                   </span>
                 </div>
                 <div className="flex flex-col">
                   <span className="text-label text-[12px]">Count</span>
                   <span className="text-sm font-semibold">
-                    {dataHistory.seat.length}
+                    {dataHistory.seats.length}
                   </span>
                 </div>
               </div>
@@ -163,9 +172,14 @@ function CardHistory({ dataHistory }) {
               </div>
               <p className="text-secondary text-[12px] font-normal text-wrap">
                 Pay this payment bill before it is due, on{" "}
-                <span className="text-importan">{dataHistory.date}</span>. If
-                the bill has not been paid by the specified time, it will be
-                forfeited
+                <span className="text-importan">
+                  {
+                    formatDateAndCheckPast(dataHistory.schedule_time)
+                      .formattedDate
+                  }
+                </span>
+                . If the bill has not been paid by the specified time, it will
+                be forfeited
               </p>
               <span className="bg-blue-primary block w-full rounded-md p-2.5 text-center text-white md:w-48">
                 Cek Pembayaran
@@ -174,52 +188,6 @@ function CardHistory({ dataHistory }) {
           )}
         </div>
       </div>
-
-      {/* if ticked paid */}
-      {/* <div className="border-background flex flex-col rounded-b-2xl border-t-1 bg-white p-5 shadow-md">
-        <div className="flex flex-col items-center justify-between gap-5 md:flex-row">
-          <div className="flex w-full flex-col items-center gap-5 md:flex-row">
-            <span className="text-active w-full rounded-lg bg-[rgba(0,186,136,0.2)] p-2.5 text-center md:w-44">
-              Ticker is Active
-            </span>
-            <span className="text-importan block w-full rounded-lg bg-[rgba(232,44,44,0.2)] p-2.5 text-center md:w-44">
-              Not paid
-            </span>
-          </div>
-          <span
-            className="text-label block cursor-pointer text-center text-nowrap"
-            onClick={() => setToggleExp(!isToggleExp)}
-          >
-            Show Details
-          </span>
-        </div>
-        <div className={`${!isToggleExp && "hidden"} mt-5 flex flex-col gap-2.5`}>
-          <div className="text-lg font-bold">Ticket Information</div>
-          <div className="flex flex-col justify-between md:flex-row">
-            <span className="text-label text-base font-light">
-              No. Rekening Virtual :
-            </span>
-            <div className="flex flex-row items-center justify-between md:gap-5">
-              <span className="text-lg font-bold">12321328913829724</span>
-              <span className="text-blue-primary border-blue-primary rounded-md border p-1">
-                copy
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-col md:flex-row md:justify-between">
-            <span className="text-secondary text-[14px]">Total Payment :</span>
-            <span className="text-blue-primary text-lg font-bold">$30</span>
-          </div>
-          <p className="text-secondary text-[12px] font-normal">
-            Pay this payment bill before it is due, on{" "}
-            <span className="text-importan">June 23, 2023</span>. If the bill
-            has not been paid by the specified time, it will be forfeited
-          </p>
-          <span className="bg-blue-primary block w-full rounded-md p-2.5 text-center text-white md:w-48">
-            Cek Pembayaran
-          </span>
-        </div>
-      </div> */}
     </div>
   );
 }
